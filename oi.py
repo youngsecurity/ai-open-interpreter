a2a6f1b0283fa5e7b8456f871ee7ad730602c0a3
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-02-22 00:45:04 -0500

    Enhanced interpreter configuration options
    Added essential API key and expanded limits for LiteLLM integration. Adjusted the interpreter to work in an offline environment with LiteLLM, requiring an API key and specific model details for improved user experience. The increase of max_tokens and context_window parameters aligns with the new LiteLLM capabilities, ensuring a more robust interaction with LM Studio.

--- oi.py
+++ oi.py
@@ -1,8 +1,12 @@
 from interpreter import interpreter
 
 interpreter.offline = True # Disables online features like Open Procedures
-interpreter.llm.model = "dolphin-mistral:latest"
-interpreter.llm.api_base = "http://10.0.255.147:11434/v1"
+interpreter.llm.api_key = "fake_key" # LiteLLM, which we use to talk to LM Studio, requires this
+interpreter.llm.model = "dolphin-mistral:latest" # makes a big difference to the experience
+interpreter.llm.api_base = "http://10.0.255.147:11434/v1" # the `/v1` is necessary
+
+interpreter.llm.max_tokens = 1000
+interpreter.llm.context_window = 3000
 
 #interpreter.chat("install ping and check if 8.8.8.8 is up") # Executes a single command
 interpreter.chat()
